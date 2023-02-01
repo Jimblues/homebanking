44b85bf309c6f7b550117cb571f5ddeaa0f8f07e
Jimblues <ericjimenez_15@hotmail.com>
2023-02-01 15:54:32 -0300

    Se agrega la relacion one-to-many con Account

--- src/main/java/com/example/homebanking/models/Client.java
+++ src/main/java/com/example/homebanking/models/Client.java
@@ -1,9 +1,7 @@
-package com.example.homebanking.entity;
+package com.example.homebanking.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.*;
 
 @Entity
 public class Client {
@@ -13,6 +11,8 @@ public class Client {
     private String firstName;
     private String lastName;
     private String email;
+    @OneToMany(mappedBy="client", fetch=FetchType.EAGER)
+    Set<Account> accounts = new HashSet<>();
 
     public Client() {
     }
@@ -47,6 +47,14 @@ public class Client {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Set<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(Set<Account> accounts) {
+        this.accounts = accounts;
     }
 
     @Override
