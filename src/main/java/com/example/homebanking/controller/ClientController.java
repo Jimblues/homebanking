3815c738f71e680281be8f7f1c2633f03bc04cce
Jimblues <ericjimenez_15@hotmail.com>
2023-02-01 15:57:31 -0300

    Se agrega el metodo getClients

--- src/main/java/com/example/homebanking/controller/ClientController.java
+++ src/main/java/com/example/homebanking/controller/ClientController.java
@@ -1,5 +1,6 @@
 package com.example.homebanking.controller;
 
+import com.example.homebanking.models.Client;
 import com.example.homebanking.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,10 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class ClientController {
     @Autowired
     private ClientService clientService;
 
+    @GetMapping("/clients")
+    public ResponseEntity<List<Client>> getClients(){
+        return new ResponseEntity<List<Client>>(clientService.getClients(), HttpStatus.OK);
+    }
 }
