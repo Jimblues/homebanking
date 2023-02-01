d470a61a360ad08dc4d7df896c1e4e945978fb4b
Jimblues <ericjimenez_15@hotmail.com>
2023-02-01 16:00:17 -0300

    Se agrega el metodo getClients

--- src/main/java/com/example/homebanking/service/ClientService.java
+++ src/main/java/com/example/homebanking/service/ClientService.java
@@ -1,6 +1,6 @@
 package com.example.homebanking.service;
 
-import com.example.homebanking.entity.Client;
+import com.example.homebanking.models.Client;
 import com.example.homebanking.repository.IClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +12,8 @@ public class ClientService {
     @Autowired
     private IClientRepository clientRepository;
 
-
+    public List<Client> getClients(){
+        return clientRepository.findAll();
+    }
 
 }
