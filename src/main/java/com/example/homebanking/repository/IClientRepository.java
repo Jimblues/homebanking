2c25d76726edc23357b2dfba317007f2576d13e0
Jimblues <ericjimenez_15@hotmail.com>
2023-02-02 13:06:34 -0300

    Se cambia int por Long

--- src/main/java/com/example/homebanking/repository/IClientRepository.java
+++ src/main/java/com/example/homebanking/repository/IClientRepository.java
@@ -2,8 +2,8 @@ package com.example.homebanking.repository;
 
 import com.example.homebanking.models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@Repository
-public interface IClientRepository extends JpaRepository<Client,Integer> {
+@RepositoryRestResource
+public interface IClientRepository extends JpaRepository<Client,Long> {
 }
