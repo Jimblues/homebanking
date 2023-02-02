393579d60174c0e8459640b0722afaade61154bb
Jimblues <ericjimenez_15@hotmail.com>
2023-02-02 13:03:36 -0300

    Se agrega un bean para agregar un cliente

--- src/main/java/com/example/homebanking/HomebankingApplication.java
+++ src/main/java/com/example/homebanking/HomebankingApplication.java
@@ -1,13 +1,22 @@
 package com.example.homebanking;
-
+import com.example.homebanking.models.Client;
+import com.example.homebanking.service.ClientService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomebankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomebankingApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner initData(ClientService repository) {
+		return (args) -> {
+			repository.saveClient(new Client("Melba","Morel","melba@mindhub.com"));
+		};
 	}
 
 }
