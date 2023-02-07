78fabdc1b90a2f9ff18f4164bdb5dc316507cf3e
Jimblues <ericjimenez_15@hotmail.com>
2023-02-06 22:50:49 -0300

    Se crea ITransactionRepository.java

--- src/main/java/com/example/homebanking/repository/ITransactionRepository.java
+++ src/main/java/com/example/homebanking/repository/ITransactionRepository.java
@@ -1,2 +1,9 @@
-package com.example.homebanking.repository;public interface ITransactionRepository {
+package com.example.homebanking.repository;
+
+import com.example.homebanking.models.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ITransactionRepository extends JpaRepository<Transaction,Long> {
 }
