2c78ca600f6d7008b7f38f8083e88304d9ce0bf3
Jimblues <ericjimenez_15@hotmail.com>
2023-02-06 22:46:49 -0300

    Se crea TransactionDTO

--- src/main/java/com/example/homebanking/dto/TransactionDTO.java
+++ src/main/java/com/example/homebanking/dto/TransactionDTO.java
@@ -1,2 +1,64 @@
-package com.example.homebanking.dto;public class TransactionDTO {
+package com.example.homebanking.dto;
+
+import com.example.homebanking.models.Account;
+import com.example.homebanking.models.Transaction;
+import com.example.homebanking.models.TransactionType;
+
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+
+public class TransactionDTO {
+    private long id;
+    private TransactionType type;
+    private double ammount;
+    private String description;
+    private LocalDateTime date;
+
+    public TransactionDTO() {
+    }
+
+    public TransactionDTO(Transaction transaction) {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+
+    public double getAmmount() {
+        return ammount;
+    }
+
+    public void setAmmount(double ammount) {
+        this.ammount = ammount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
 }
