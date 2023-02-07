916ecbd47483c82b59ab44f5df9718ed31989f82
Jimblues <ericjimenez_15@hotmail.com>
2023-02-06 22:52:32 -0300

    Se agregan los metodos getAccounts y getAccountById

--- src/main/java/com/example/homebanking/service/AccountService.java
+++ src/main/java/com/example/homebanking/service/AccountService.java
@@ -1,12 +1,24 @@
 package com.example.homebanking.service;
 
+import com.example.homebanking.models.Account;
 import com.example.homebanking.repository.IAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AccountService {
     @Autowired
     private IAccountRepository accountRepository;
+    public Account saveAccount(Account acc){
+        return accountRepository.save(acc);
+    }
+    public List<Account> getAccounts(){
+        return accountRepository.findAll();
+    }
+    public Account getAccountById(long id){
+        return accountRepository.findById(id).get();
+    }
 
 }
