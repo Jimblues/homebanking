79d325e62e934d645fb8dd8d426b4831a42a4c10
Jimblues <ericjimenez_15@hotmail.com>
2023-02-06 22:45:08 -0300

    Se modifican los metodos getClients y getClient

--- src/main/java/com/example/homebanking/controller/ClientController.java
+++ src/main/java/com/example/homebanking/controller/ClientController.java
@@ -1,5 +1,7 @@
 package com.example.homebanking.controller;
 
+import com.example.homebanking.dto.ClientDTO;
+import com.example.homebanking.models.Account;
 import com.example.homebanking.models.Client;
 import com.example.homebanking.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequestMapping("/api")
@@ -16,8 +22,18 @@ public class ClientController {
     private ClientService clientService;
 
     @GetMapping("/clients")
-    public ResponseEntity<List<Client>> getClients(){
-        return new ResponseEntity<List<Client>>(clientService.getClients(), HttpStatus.OK);
+    public ResponseEntity<List<ClientDTO>> getClients(){
+        return new ResponseEntity<List<ClientDTO>>((List<ClientDTO>) clientService.getClients().stream().map(client -> new ClientDTO(client)).collect(toList()), HttpStatus.OK);
+    }
+    @GetMapping("/clients/{id}")
+    public ResponseEntity<ClientDTO> getClient(@PathVariable long id){
+        return new ResponseEntity<ClientDTO>(new ClientDTO(clientService.getClientById(id)), HttpStatus.OK);
+    }
+    @GetMapping("/clientsAcc/{id}")
+    public ResponseEntity<Set<Account>> lstAccounts(@PathVariable long id){
+        Client c = clientService.getClientById(id);
+        System.out.println(c.getAccounts());
+        return new ResponseEntity<Set<Account>>(c.getAccounts(),HttpStatus.OK);
     }
     @PostMapping("/addClient")
     public ResponseEntity<?> saveClient(@RequestBody Client c){
