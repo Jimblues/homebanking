52cfd3763e65ecec165d85024d0431eefd9d1268
Jimblues <ericjimenez_15@hotmail.com>
2023-02-06 22:47:53 -0300

    Se agrega relacion OneToMany con Transaction.java

--- src/main/java/com/example/homebanking/models/Account.java
+++ src/main/java/com/example/homebanking/models/Account.java
@@ -1,28 +1,41 @@
 package com.example.homebanking.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private long id;
     private String number;
     private LocalDateTime creationDate;
     private double balance;
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "client_id")
     private Client client;
+    @OneToMany(mappedBy = "account",fetch = FetchType.EAGER)
+    Set<Transaction> transactions = new HashSet<>();
 
     public Account() {
     }
 
-    public int getId() {
+    public Account(String number, LocalDateTime creationDate, double balance, Client client) {
+        this.number = number;
+        this.creationDate = creationDate;
+        this.balance = balance;
+        this.client = client;
+    }
+
+    public long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
 
@@ -49,13 +62,21 @@ public class Account {
     public void setBalance(double balance) {
         this.balance = balance;
     }
-
+    @JsonIgnore
     public Client getClient() {
         return client;
     }
 
     public void setClient(Client client) {
         this.client = client;
+    }
+
+    public Set<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(Set<Transaction> transaction) {
+        this.transactions = transaction;
     }
 
     @Override
