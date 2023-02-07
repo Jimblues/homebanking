ac6aeb828a7eec64c743403bfb5fd670a0257b5e
Jimblues <ericjimenez_15@hotmail.com>
2023-02-06 22:53:06 -0300

    Se crea TransactionService.java

--- src/main/java/com/example/homebanking/service/TransactionService.java
+++ src/main/java/com/example/homebanking/service/TransactionService.java
@@ -1,2 +1,20 @@
-package com.example.homebanking.service;public class TransactionService {
+package com.example.homebanking.service;
+
+import com.example.homebanking.models.Transaction;
+import com.example.homebanking.models.TransactionType;
+import com.example.homebanking.repository.ITransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionService {
+    @Autowired
+    private ITransactionRepository transactionRepository;
+
+    public Transaction getTransactionById(long id){
+        return transactionRepository.findById(id).get();
+    }
+    public Transaction saveTransaction(Transaction transaction){
+        return transactionRepository.save(transaction);
+    }
 }
